1341d349a062ad94cb135bd12f3a011bd57b12ce
XuyenLe <xuyenleit@gmail.com>
2024-12-30 16:08:03 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import DemoComponent from './DemoComponent';
 
 function App() {
+  const [show ,setShow] = useState(false)
+  const [title,setTitle] = useState("home page")
+  const [data,setData] = useState([])
+  const [url,setUrl] = useState("posts")
+  // 1.UseEffect duoc goi sau khi component duoc mount vao dom va render
+  // ra UI
+  // 2.UseEffect de xu ly cac 
+  // side Effect(lam viec voi API,cap nhat lai DOM,settimeout,setInterval)
+  // 3. voi dependence [] thi useEffect chi goi 1 lan duy nhat 
+  // sau khi component mount vao dom
+  // 4. Effect duoc goi moi khi dependence thay doi
+  useEffect(()=>{
+    console.log("goi useEffect");
+    document.title = title
+  },[title])
+useEffect(()=>{
+  fetch(`https://jsonplaceholder.typicode.com/${url}`)
+    .then(res=>res.json())
+    .then(result=>setData(result))
+    .catch(error=>console.log(error))
+},[url])
+  console.log("data: ",data);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Demo useEffect </h1>
+      <input placeholder='Enter title' onChange={(e)=>setTitle(e.target.value)}/>
+      <button onClick={()=>setShow(!show)}>Show</button>
+      {show && <DemoComponent/>}
+    <div>
+    <button onClick={()=>setUrl("posts")}
+       style={url=="posts"?{background:"black",color:"white"}:{}}>Post</button>
+    <button onClick={()=>setUrl("albums")} 
+      style={url=="albums"?{background:"black",color:"white"}:{}}>Albums</button>
+    </div>
+      <ul>
+        {data.length > 0 && data.map((item,index)=>{
+          return (<li key={index}>{item.title}</li>)
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/DemoComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DemoComponent(props) {
+    return (
+        <div>
+            <h1>Day la component Demo</h1>
+        </div>
+    );
+}
+
+export default DemoComponent;
